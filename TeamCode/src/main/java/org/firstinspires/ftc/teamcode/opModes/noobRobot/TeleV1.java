7f0c87e923ea4fa68a9f89652a2e10b258adef65
Paul Berendt <paulberendt@aurora-schools.org>
2024-05-08 21:12:10 -0400

    Paul B testing turtle bot. We made good progress, set the motors correctly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/noobRobot/TeleV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/noobRobot/TeleV1.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Config
 @TeleOp(name= "Positive-Protons" , group = "advanced")
@@ -21,26 +21,45 @@ public class TeleV1 extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "front-right");
         backLeft = hardwareMap.get(DcMotor.class, "back-left");
         backRight = hardwareMap.get(DcMotor.class, "back-right");
+        //Setting Motors
 
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        //Making them move in the correct direction
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive() && !isStopRequested()) {
-
-            double x = gamepad1.left_stick_y;
-            double y = gamepad1.left_stick_x;
-            double rx = 0;
-
+        //The loop
+            double y = -gamepad1.right_stick_x;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.left_stick_y;
+        //Inputs
             frontLeft.setPower(y + x + rx);
             backLeft.setPower(y - x + rx);
             frontRight.setPower(y - x - rx);
             backRight.setPower(y + x - rx);
+
+
+            if (gamepad2.a){
+                backRight.setPower(1);
+            }
+            if (gamepad2.b){
+                backLeft.setPower(-1);
+            }
+            if (gamepad2.x){
+                frontRight.setPower(-1);
+            }
+            if (gamepad2.y){
+                frontLeft.setPower(1);
+            }
         }
     }
 }
